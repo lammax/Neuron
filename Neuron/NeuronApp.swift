c95efb88d8dea47d01189ce87285e447ae387739
Lammax <lammax@mail.ru>
2020-10-07 19:03:06 +0300

    Initial Commit

---
+++ Neuron/NeuronApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuronApp.swift
+//  Neuron
+//
+//  Created by Mac on 07.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct NeuronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
