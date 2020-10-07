8362a1cec9d668f23651b5f39259a69a34a48d61
Lammax <lammax@mail.ru>
2020-10-07 19:19:38 +0300

    Neuron train preparations

---
+++ Lib/Neurons/Neuron.swift
@@ -0,0 +1,42 @@
+//
+//  Neuron.swift
+//  Neuron
+//
+//  Created by Mac on 07.10.2020.
+//
+
+import Foundation
+
+class Neuron {
+    
+    private var weight: Double
+    private var lastError: Double
+    private var smoothing: Double
+    
+    init() {
+        weight = 0.5
+        lastError = .zero
+        smoothing = 0.0000001
+    }
+    
+    func set(smoothing: Double) {
+        self.smoothing = smoothing
+    }
+    
+    func process(input: Double) -> Double {
+        input * weight
+    }
+    
+    func restore(output: Double) -> Double {
+        output / weight
+    }
+    
+    func train(input: Double, expectedResult: Double) -> Double {
+        let actualResult = input * weight
+        lastError = expectedResult - actualResult
+        let correction = (lastError / actualResult) * smoothing
+        weight += correction
+        return lastError
+    }
+    
+}
